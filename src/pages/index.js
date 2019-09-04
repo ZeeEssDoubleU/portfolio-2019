91e90be2530c48e1d0d06716046afd863502e84f
Zak Williams <zak.williams2287@gmail.com>
2019-09-03 22:51:30 -0400

    moved About.js back to correct locaiton with updated filename

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { theme } from "../styles/theme"
 // import components
 import Layout from "../components/Layout/Layout"
 import Landing from "../components/Landing"
-import About from "../components/Projects/About"
+import About from "../components/About"
 import Projects from "../components/Projects/Projects"
 import Contact from "../components/Contact/Contact"
 
