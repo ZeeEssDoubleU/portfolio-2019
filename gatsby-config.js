8dc571ee644467b2d45186c76d362a2bc67c1cfe
Zak Williams <zak.williams2287@gmail.com>
2019-09-20 02:27:57 -0400

    Added || (or) statements to firebase env vars for proper api integration when accessing from web.app domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ require("dotenv/config")
 
 module.exports = {
   siteMetadata: {
-    title: `Zachary Williams | Portfolio`,
+    title: `Web Developer | Zachary Williams | New York City`,
   },
   plugins: [
     {
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,13 +2,20 @@ import firebase from "firebase/app"
 import "firebase/database"
 
 const config = {
-  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
-  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
-  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.GATSBY_FIREBASE_APP_ID,
+  apiKey: 
+    process.env.GATSBY_FIREBASE_API_KEY || process.env.FIREBASE_API_KEY,
+  authDomain:
+    process.env.GATSBY_FIREBASE_AUTH_DOMAIN || process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL:
+    process.env.GATSBY_FIREBASE_DATABASE_URL || process.env.FIREBASE_DATABASE_URL,
+  projectId:
+    process.env.GATSBY_FIREBASE_PROJECT_ID || process.env.FIREBASE_PROJECT_ID,
+  storageBucket:
+    process.env.GATSBY_FIREBASE_STORAGE_BUCKET || process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID || process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: 
+    process.env.GATSBY_FIREBASE_APP_ID || process.env.FIREBASE_APP_ID,
 }
 
 export const firebaseInit = () => {
