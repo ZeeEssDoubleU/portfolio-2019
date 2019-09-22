3c23899b5c1221ce0789157228855b0fd02570c1
Zak Williams <zak.williams2287@gmail.com>
2019-09-22 01:50:57 -0400

    updated dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,7 @@
 // dotenv needed to protect contentful API keys
-require("dotenv/config")
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv/config")
+}
 
 module.exports = {
   siteMetadata: {
