871313bd9669eb2847bba21048eccd1b911b979b
Zak Williams <zak.williams2287@gmail.com>
2019-11-05 21:56:39 -0500

    fixed linkedin profil link

--- src/components/elements/Social.js
+++ src/components/elements/Social.js
@@ -4,7 +4,6 @@ import styled, { withTheme } from "styled-components"
 import Icon from "../Icons/Icon"
 import { ExternalLink } from "./CustomLink"
 
-
 // ***COMPONENT***
 const Social = props => {
   return (
@@ -25,7 +24,7 @@ const Social = props => {
         className="social-link"
         tabIndex={0}
         aria-label="linkedin profile"
-        href="https://www.linkedin.com"
+        href="https://www.linkedin.com/in/ZacharyScottWilliams"
       >
         <Inner>
           <Icon name="linkedin" className="social-icon" />
@@ -58,47 +57,47 @@ const Social = props => {
 }
 export default withTheme(Social)
 
-  // ***STYLES***
-  const Container = styled.div`
-    display: flex;
-    margin: 20px auto;
-    height: fit-content;
-    flex-wrap: wrap;
-    justify-content: center;
-    .social-link {
-      position: relative;
-      background: linear-gradient(
-        to left,
-        hsla(${props => props.theme.appGreenPartial}, 0.5),
-        hsla(${props => props.theme.appBluePartial}, 0.5)
-      );
-      padding: 1px;
-      border-radius: 50%;
-      margin: 10px 10px;
-      cursor: pointer;
-      transition: transform 0.2s;
-      &:hover,
-      &:active {
-        transform: scale(1.2);
-      }
-      @media (min-width: ${props => props.theme.tablet + "px"}) {
-        margin: 10px 20px;
-      }
-    }
-  `
-  const Inner = styled.div`
-    background: ${props => props.theme.appBgDark};
-    height: 38px;
-    width: 38px;
+// ***STYLES***
+const Container = styled.div`
+  display: flex;
+  margin: 20px auto;
+  height: fit-content;
+  flex-wrap: wrap;
+  justify-content: center;
+  .social-link {
+    position: relative;
+    background: linear-gradient(
+      to left,
+      hsla(${props => props.theme.appGreenPartial}, 0.5),
+      hsla(${props => props.theme.appBluePartial}, 0.5)
+    );
+    padding: 1px;
     border-radius: 50%;
-    .social-icon {
-      position: absolute;
-      /* center element */
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      height: 18px;
-      width: 18px;
-      color: ${props => props.theme.appGreen};
+    margin: 10px 10px;
+    cursor: pointer;
+    transition: transform 0.2s;
+    &:hover,
+    &:active {
+      transform: scale(1.2);
     }
-  `
+    @media (min-width: ${props => props.theme.tablet + "px"}) {
+      margin: 10px 20px;
+    }
+  }
+`
+const Inner = styled.div`
+  background: ${props => props.theme.appBgDark};
+  height: 38px;
+  width: 38px;
+  border-radius: 50%;
+  .social-icon {
+    position: absolute;
+    /* center element */
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 18px;
+    width: 18px;
+    color: ${props => props.theme.appGreen};
+  }
+`
